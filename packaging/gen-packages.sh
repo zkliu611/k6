15dbcdbf2c6c66922a21bae3264e801d3d650091
Luiz Filho <luizbafilho@gmail.com>
2018-06-05 16:09:27 -0300

    Adding build directly to circle ci config

--- packaging/gen-packages.sh
+++ packaging/gen-packages.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
+if [[ -z "${VERSION}" ]]; then
+    echo "\$VERSION enviroment variable undefined"
+    exit 2
+fi
+
 rm -rf pkg-build
 
 echo "Building k6..."
-GOARCH=$ARCH go build -o /tmp/k6
+CGO_ENABLED=0 GOARCH=$ARCH go build -a -ldflags '-s -w' -o /tmp/k6
 echo "Done!"
 
 go-bin-$1 generate --file packaging/$1.json -a $ARCH --version $VERSION -o dist/k6-v$VERSION-$ARCH.deb
