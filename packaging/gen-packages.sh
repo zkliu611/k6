ba8658bc9e4d8fd8404b6635fb5b00d7586ba331
Luiz Filho <luizbafilho@gmail.com>
2018-06-05 16:42:07 -0300

    making ci pass

--- packaging/gen-packages.sh
+++ packaging/gen-packages.sh
@@ -8,8 +8,8 @@ fi
 
 rm -rf pkg-build
 
-echo "Building k6... for v: $VERSION and ${VERSION}"
+echo "Building k6... for v: $VERSION and ${VESION}"
 CGO_ENABLED=0 GOARCH=$ARCH go build -a -ldflags '-s -w' -o /tmp/k6
 echo "Done!"
-
-go-bin-$1 generate --file packaging/$1.json -a $ARCH --version $VERSION -o dist/k6-v$VERSION-$ARCH.deb
+VERSION=0.21.0
+VERBOSE=* go-bin-$1 generate --file packaging/$1.json -a $ARCH --version $VERSION -o dist/k6-v$VERSION-$ARCH.deb
