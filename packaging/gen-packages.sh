d6e1d512cfa1abfbe50260edf5dee1ec4e4a6784
Luiz Filho <luizbafilho@gmail.com>
2018-06-12 14:40:06 -0300

    Making making ci pass

--- packaging/gen-packages.sh
+++ packaging/gen-packages.sh
@@ -13,10 +13,6 @@ fi
 
 rm -rf pkg-build
 
-echo "Building k6..."
-CGO_ENABLED=0 GOARCH=$ARCH go build -a -ldflags '-s -w' -o /tmp/k6
-echo "Done!"
-
 mkdir -p dist
 go-bin-deb generate --file packaging/deb.json -a $ARCH --version $VERSION -o dist/k6-v$VERSION-$ARCH.deb
 go-bin-rpm generate --file packaging/rpm.json -a $ARCH --version $VERSION -o dist/k6-v$VERSION-$ARCH.rpm
